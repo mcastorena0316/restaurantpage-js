588d4f744acc19c279f9f66503436a98b7271ee9
Melissa Castorena <mcastorena0316@gmail.com>
2020-03-28 13:10:18 -0600

    main section with Javascript done

--- src/images-section.js
+++ src/images-section.js
@@ -1,18 +1,18 @@
 function imageSection() {
   const imageSec = document.createElement('div');
-  imageSec.innerHTML = ` <div class="image image1">
+  imageSec.innerHTML = ` <div class="image image1" id="image image1">
                                 <p>"My favorite restaurant in all the world"</p>
                                 <p>-Michelle Obama</p>
                             </div>
-                            <div class="image image2">
+                            <div class="image image2" id="image image2">
                                 <p>"Definitely a must when you're in town"</p>
                                 <p>-Elon Musk</p>
                             </div>
-                            <div class="image image3">
+                            <div class="image image3" id="image image3">
                                 <p>"I had the idea for Star Wars here!"</p>
                                 <p>-George Lucas</p>
                             </div>
-                            <div class="image image4">
+                            <div class="image image4" id="image image4">
                                 <p>"I wrote my first hit song in a napkin when I was eating a hamburger in this place!</p>
                                 <p>-Lady Gaga</p>
                             </div>`;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 import footer from './footer';
 import header from './header';
 import imageSection from './images-section';
@@ -7,3 +8,21 @@ const content = document.getElementById('content');
 content.appendChild(header());
 content.appendChild(imageSection());
 content.appendChild(footer());
+
+const images = document.querySelectorAll('.image');
+
+
+function biggerImageandText() {
+  if (document.querySelectorAll('.open-img').length === 0) {
+    this.classList.toggle('open-img');
+  } else {
+    const els = document.querySelectorAll('.open-img');
+    console.log(els)
+    for (let i = 0; i < els.length; i += 1) {
+      els[i].classList.remove('open-img');
+    }
+  }
+}
+
+
+images.forEach(image => image.addEventListener('click', biggerImageandText));
